3e688f6ea415ad03a328d7668209c55b7f1392a1
Jonathan McElroy <jonathan_mcelroy@baylor.edu>
2014-10-16 04:13:35 -0500

    More comments

--- src/nudat/protocol/NuDatException.java
+++ src/nudat/protocol/NuDatException.java
@@ -1,23 +1,48 @@
 package nudat.protocol;
 
+/**
+ * NuDatException
+ *
+ * October 16, 2014
+ *
+ * @author Jonathan McElroy
+ * @version 0.2
+ */
 public class NuDatException extends Exception {
     ////////////////////
     // Members
     ////////////////////
 
+    /**
+     * ID to serialization
+     */
     private static final long serialVersionUID = 1L;
 
-    ErrorCode errorCode;
+    /**
+     * The error code associated with the exception
+     */
+    private ErrorCode errorCode;
 
     ////////////////////
     // Contructors
     ////////////////////
 
+    /**
+     * Create the exception from an ErrorCode
+     *
+     * @param errorCode
+     */
     public NuDatException(ErrorCode errorCode){
         super(errorCode.getErrorMessage());
         this.errorCode = errorCode;
     }
 
+    /**
+     * Create an exception from an ErrorCode and a cause
+     *
+     * @param errorCode
+     * @param cause
+     */
     public NuDatException(ErrorCode errorCode, Throwable cause) {
         super(cause);
         this.errorCode = errorCode;
@@ -27,6 +52,11 @@ public class NuDatException extends Exception {
     // Methods
     ////////////////////
 
+    /**
+     * Get the error code associated with the exception
+     *
+     * @return
+     */
     public ErrorCode getErrorCode() {
         return this.errorCode;
     }
