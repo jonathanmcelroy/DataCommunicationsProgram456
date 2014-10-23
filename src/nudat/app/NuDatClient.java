88471a3fdba96ca9f00a2278dbc1e3ec84d50f05
Jonathan McElroy <jonathan_mcelroy@baylor.edu>
2014-10-23 10:07:39 -0500

    Final changes

--- src/nudat/app/NuDatClient.java
+++ src/nudat/app/NuDatClient.java
@@ -36,6 +36,16 @@ public class NuDatClient {
     private static final int MAX_QUERY_ID = 1000000;
 
     /**
+     * The timeout for a single connection
+     */
+    private static final int TIMEOUT = 3000;
+
+    /**
+     * The maximum amount of time to wait for a response
+     */
+    private static final int MAX_TIMEOUT = 15000;
+
+    /**
      * Random number generator for queryId
      */
     private static final Random random = new Random();
@@ -45,12 +55,13 @@ public class NuDatClient {
      *
      * @param args
      *      Args from the command line. Must be of the form: java NuDatClient
-     *      serverIP serverPost postNumber
+     *      serverIP serverPort postNumber
      */
     public static void main(String[] args) {
         // Check for correct usage
         if(args.length != 3) {
-            System.err.println("USAGE: java NuDatClient serverIP serverPort postNumber");
+            System.err.println(
+                "USAGE: java NuDatClient serverIP serverPort postNumber");
             System.exit(1);
         }
 
@@ -58,14 +69,27 @@ public class NuDatClient {
         String serverIP = args[0];
 
         // Get the port the server is running on
-        int serverPort = Integer.parseInt(args[1]);
+        int serverPort = 0;
+        try {
+            serverPort = Integer.parseInt(args[1]);
+        }
+        catch(NumberFormatException e) {
+            System.out.println("Port must be a valid integer");
+        }
 
         // Get the number of posts the user wants
-        int postNumber = Integer.parseInt(args[2]);
+        int postNumber = 0;
+        try {
+            postNumber = Integer.parseInt(args[2]);
+        }
+        catch(NumberFormatException e) {
+            System.out.println("Post number must be a valid integer");
+        }
 
         try(DatagramSocket udpServerSocket = new DatagramSocket()) {
             // Create a query from the arguments
-            NuDatQuery query = new NuDatQuery(random.nextInt(MAX_QUERY_ID), postNumber);
+            NuDatQuery query = new NuDatQuery(
+                random.nextInt(MAX_QUERY_ID), postNumber);
 
             // Try to connect to the address. This does not set up a connection,
             // it just validates the serverIP and makes sure the socket will
@@ -80,37 +104,51 @@ public class NuDatClient {
             }
 
             // set the socket timeout to 3 seconds
-            udpServerSocket.setSoTimeout(3000);
+            udpServerSocket.setSoTimeout(TIMEOUT);
 
             // the array to hold the returned message
             NuDatMessage message = null;
 
+            long startTime = System.currentTimeMillis();
             boolean received = false;
-            while(!received) {
+            while(!received &&
+                    System.currentTimeMillis() - startTime < MAX_TIMEOUT) {
                 // send a request for posts
                 sendQuery(query, udpServerSocket);
                 try {
                     // get the response. If it doesn't return after 3 seconds,
                     // raise a socket timeout exception
-                    message = NuDatMessage.decode(receiveResponse(udpServerSocket));
-                    received  = true;
+                    message = NuDatMessage.decode(receiveResponse(
+                                                      udpServerSocket));
+                    if(message.getQueryId() == query.getQueryId()) {
+                        received  = true;
+                    }
                 }
                 catch(SocketTimeoutException e) {}
             }
 
+            // if we have not received the correct packet in the amount of time
+            // set
+            if(!received) {
+                // we could not communicate with the server
+                throw new IOException("Could not communicate with the server");
+            }
+
+            // if the message received is not a response
             if(!(message instanceof NuDatResponse)) {
                 throw new NuDatException(ErrorCode.UNEXPECTEDPACKETTYPE);
             }
 
+            // cast the message to a response
             NuDatResponse response = (NuDatResponse)message;
 
+            // print out each post
             for(String post : response.getPosts()) {
                 System.out.println(post);
             }
         }
         catch(IOException e) {
-            e.printStackTrace();
-            System.err.println("Could not communicate to the server");
+            System.err.println("Problem communicating with the server");
             System.exit(1);
         }
         catch(NuDatException e) {
@@ -127,7 +165,8 @@ public class NuDatClient {
      * @param udpServerSocket
      *      is the socket to send the packet through
      */
-    private static void sendQuery(NuDatQuery query, DatagramSocket udpServerSocket)
+    private static void sendQuery(NuDatQuery query,
+                                  DatagramSocket udpServerSocket)
     throws IOException, NuDatException {
         // the message to send
         byte[] queryMessage = query.encode();
@@ -168,10 +207,4 @@ public class NuDatClient {
         return received;
     }
 }
-
-
-
-
-
-
 
